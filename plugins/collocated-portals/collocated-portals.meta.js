fb915f10715ef5bea1d5ef7189224bdadd5e921e
Mike H 57Cell <mike40033@gmail.com>
2019-10-12 21:47:56 +0800

    update metadata in plugin

---
+++ plugins/collocated-portals/collocated-portals.meta.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @id             iitc-plugin-collocated-portals@57Cell
+// @name           IITC plugin: Collocated Portals
+// @category       Info
+// @version        0.0.1.20101012.21732
+// @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
+// @updateURL      https://github.com/mike40033/iitc-57Cell/raw/master/plugins/collocated-portals/collocated-portals.meta.js
+// @downloadURL    https://github.com/mike40033/iitc-57Cell/raw/master/plugins/collocated-portals/collocated-portals.user.js
+// @description    Find collocated portals
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @include        https://*.ingress.com/collcation/*
+// @include        http://*.ingress.com/mission/*
+// @match          https://*.ingress.com/mission/*
+// @match          http://*.ingress.com/mission/*
+// @grant          none
+// ==/UserScript==
+
+
--- plugins/collocated-portals/collocated-portals.user.js
+++ plugins/collocated-portals/collocated-portals.user.js
@@ -2,16 +2,16 @@
 // @id             iitc-plugin-collocated-portals@57Cell
 // @name           IITC plugin: Collocated Portals
 // @category       Info
-// @version        0.1.2.20170108.21732
+// @version        0.0.1.20101012.21732
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
-// @updateURL      https://www.teasearch.com/SS/collocated.meta.js
-// @downloadURL    https://www.teasearch.com/SS/collocated.user.js
+// @updateURL      https://github.com/mike40033/iitc-57Cell/raw/master/plugins/collocated-portals/collocated-portals.meta.js
+// @downloadURL    https://github.com/mike40033/iitc-57Cell/raw/master/plugins/collocated-portals/collocated-portals.user.js
 // @description    Find collocated portals
 // @include        https://*.ingress.com/intel*
 // @include        http://*.ingress.com/intel*
 // @match          https://*.ingress.com/intel*
 // @match          http://*.ingress.com/intel*
-// @include        https://*.ingress.com/mission/*
+// @include        https://*.ingress.com/collcation/*
 // @include        http://*.ingress.com/mission/*
 // @match          https://*.ingress.com/mission/*
 // @match          http://*.ingress.com/mission/*
