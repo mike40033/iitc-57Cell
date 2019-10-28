896ea04adac1821c8df2b0219f878d20faf95c17
mxxcon <847140+mxxcon@users.noreply.github.com>
2019-10-28 02:46:04 -0400

    Update collocated-portals.user.js
    Remove unnecessary `@include` and `@match` lines.
    No need to include `http://` links since server automatically redirects everything to `https://`
    Furthermore, there's no need for this script to run on `/missions` url. 
    Finally, it had `/*` which would override any other entries and was unnecessarily broad.

--- plugins/collocated-portals/collocated-portals.user.js
+++ plugins/collocated-portals/collocated-portals.user.js
@@ -8,17 +8,7 @@
 // @downloadURL    https://github.com/mike40033/iitc-57Cell/raw/master/plugins/collocated-portals/collocated-portals.user.js
 // @description    Find collocated portals
 // @include        https://*.ingress.com/intel*
-// @include        http://*.ingress.com/intel*
 // @match          https://*.ingress.com/intel*
-// @match          http://*.ingress.com/intel*
-// @include        https://*.ingress.com/*
-// @include        http://*.ingress.com/*
-// @match          https://*.ingress.com/*
-// @match          http://*.ingress.com/*
-// @include        https://*.ingress.com/collcation/*
-// @include        http://*.ingress.com/mission/*
-// @match          https://*.ingress.com/mission/*
-// @match          http://*.ingress.com/mission/*
 // @grant          none
 // ==/UserScript==
 
