426961c7010a69ce48a69abdf5aa3a0d8549b5ee
Michael Hartley <mike40033@gmail.com>
2020-07-03 16:35:13 +0800

    Revert "Remove extra include/match from collocated-portals.user.js"

--- plugins/collocated-portals/collocated-portals.user.js
+++ plugins/collocated-portals/collocated-portals.user.js
@@ -7,8 +7,18 @@
 // @updateURL      https://github.com/mike40033/iitc-57Cell/raw/master/plugins/collocated-portals/collocated-portals.meta.js
 // @downloadURL    https://github.com/mike40033/iitc-57Cell/raw/master/plugins/collocated-portals/collocated-portals.user.js
 // @description    Find collocated portals
-// @include        https://intel.ingress.com/*
-// @match          https://intel.ingress.com/*
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @include        https://*.ingress.com/*
+// @include        http://*.ingress.com/*
+// @match          https://*.ingress.com/*
+// @match          http://*.ingress.com/*
+// @include        https://*.ingress.com/collcation/*
+// @include        http://*.ingress.com/mission/*
+// @match          https://*.ingress.com/mission/*
+// @match          http://*.ingress.com/mission/*
 // @grant          none
 // ==/UserScript==
 
