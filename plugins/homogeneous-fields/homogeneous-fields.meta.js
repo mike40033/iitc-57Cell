f79f4c81643807efe750ecb3938d63d618f88b00
Michael Hartley <mike40033@gmail.com>
2023-05-21 21:03:49 +0800

    Create meta file for the Homogeneous Fields plugin

---
+++ plugins/homogeneous-fields/homogeneous-fields.meta.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @id            iitc-plugin-homogeneous-fields@57Cell
+// @name         Homogeneous Fields
+// @version      1.0.0.20230521
+// @description  Plugin for planning HCF in IITC
+// @author       57Cell (Michael Hartley) and ChatGPT 4.0
+// @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
+// @updateURL      https://github.com/mike40033/iitc-57Cell/raw/master/plugins/homogeneous-fields/homogeneous-fields.meta.js
+// @downloadURL    https://github.com/mike40033/iitc-57Cell/raw/master/plugins/homogeneous-fields/homogeneous-fields.user.js
+// @include        https://intel.ingress.com/*
+// @include        http://intel.ingress.com/*
+// @match          https://intel.ingress.com/*
+// @match          http://intel.ingress.com/*
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @include        https://*.ingress.com/mission/*
+// @include        http://*.ingress.com/mission/*
+// @match          https://*.ingress.com/mission/*
+// @match          http://*.ingress.com/mission/*
+// @grant        none
+// ==/UserScript==
