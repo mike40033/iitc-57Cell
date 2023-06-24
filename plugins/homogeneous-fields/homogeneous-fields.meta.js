b34073ec87c936bd96787f629fdda453a9413979
Heistergand <info@heistergand.com>
2023-06-24 18:58:24 +0200

    Update homogeneous-fields.meta.js
    Add plugin layer and link drawings.

--- plugins/homogeneous-fields/homogeneous-fields.meta.js
+++ plugins/homogeneous-fields/homogeneous-fields.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @id            iitc-plugin-homogeneous-fields@57Cell
 // @name         IITC Plugin: Homogeneous Fields
-// @version      1.0.0.20230521
+// @version      1.1.0.20230624
 // @description  Plugin for planning HCF in IITC
 // @author       57Cell (Michael Hartley) and ChatGPT 4.0
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
