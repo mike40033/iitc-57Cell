18ca6165fa96ca97fd664b8d48697f2febcb35b7
Mike H 57Cell <mike40033@gmail.com>
2023-07-23 15:25:38 +0800

    update version number

--- plugins/homogeneous-fields/homogeneous-fields.meta.js
+++ plugins/homogeneous-fields/homogeneous-fields.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @id            iitc-plugin-homogeneous-fields@57Cell
 // @name         IITC Plugin: Homogeneous Fields
-// @version      1.2.2.20230715
+// @version      1.2.3.20230723
 // @description  Plugin for planning HCF in IITC
 // @author       57Cell (Michael Hartley) and ChatGPT 4.0
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
