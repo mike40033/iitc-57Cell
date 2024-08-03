e5baf2516f24f442cbb45a892bbf37538fe8e116
Michael Hartley <mike40033@gmail.com>
2024-08-03 20:09:11 +0800

    Create link-and-field-counter.user.js (#27)
    * Create link-and-field-counter.user.js
    
    * Create link-and-field-counter.meta.js
    
    * Update link-and-field-counter.user.js

---
+++ plugins/link-and-field-counter/link-and-field-counter.meta.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @id             iitc-plugin-homogeneous-fields@57Cell
+// @name           IITC Plugin: 57Cell's Link And Field Counter
+// @version        0.4.0.20240803
+// @description    Plugin for counting links and fields by agents
+// @author         57Cell (Michael Hartley) and Claude.AI
+// @category       Layer
+// @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
+// @updateURL      https://github.com/mike40033/iitc-57Cell/raw/master/plugins/link-and-field-counter/link-and-field-counter.meta.js
+// @downloadURL    https://github.com/mike40033/iitc-57Cell/raw/master/plugins/link-and-field-counter/link-and-field-counter.user.js
+// @include        https://intel.ingress.com/*
+// @include        http://intel.ingress.com/*
+// @match          https://intel.ingress.com/*
+// @match          http://intel.ingress.com/*
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @include        https://*.ingress.com/mission/*
+// @include        http://*.ingress.com/mission/*
+// @match          https://*.ingress.com/mission/*
+// @match          http://*.ingress.com/mission/*
+// @grant        none
+// ==/UserScript==
