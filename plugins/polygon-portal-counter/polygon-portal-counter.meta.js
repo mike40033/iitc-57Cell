e67164de223bbbd8ba7826a2851f6c983ba8d012
Michael Hartley <mike40033@gmail.com>
2024-08-03 10:11:50 +0800

    Create polygon-portal-counter.meta.js

---
+++ plugins/polygon-portal-counter/polygon-portal-counter.meta.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @id             iitc-plugin-homogeneous-fields@57Cell
+// @name           IITC Plugin: 57Cell's Polygon Portal Counter
+// @version        0.4.0.20240803
+// @description    Plugin for counting portals in polygons
+// @author         57Cell (Michael Hartley) and Claude.AI
+// @category       Layer
+// @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
+// @updateURL      https://github.com/mike40033/iitc-57Cell/raw/master/plugins/polygon-portal-counter/polygon-portal-counter.meta.js
+// @downloadURL    https://github.com/mike40033/iitc-57Cell/raw/master/plugins/polygon-portal-counter/polygon-portal-counter.user.js
+// @include        https://intel.ingress.com/*
+// @include        http://intel.ingress.com/*
+// @match          https://intel.ingress.com/*
+// @match          http://intel.ingress.com/*
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @include        https://*.ingress.com/mission/*
+// @include        http://*.ingress.com/mission/*
+// @match          https://*.ingress.com/mission/*
+// @match          http://*.ingress.com/mission/*
+// @grant        none
+// ==/UserScript==
