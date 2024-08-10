76ed5b48eaf29bae1da05a8020ef5f78091588a7
Michael Hartley <mike40033@gmail.com>
2024-08-10 17:17:19 +0800

    Create Image Overlay Plugin
    * Create Image Overlay plugin
    
    * Create image-overlay.meta.js

---
+++ plugins/image-overlay/image-overlay.meta.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @id             iitc-plugin-image-overlay@57Cell
+// @name           IITC Plugin: 57Cell's Image Overlay
+// @version        0.1.4.20240807
+// @description    Plugin for overlaying and adjusting images on the map
+// @author         57Cell (Michael Hartley) and Claude.AI
+// @category       Layer
+// @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
+// @updateURL      https://github.com/mike40033/iitc-57Cell/raw/master/plugins/image-overlay/image-overlay.meta.js
+// @downloadURL    https://github.com/mike40033/iitc-57Cell/raw/master/plugins/image-overlay/image-overlay.user.js
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @include        https://*.ingress.com/mission/*
+// @include        http://*.ingress.com/mission/*
+// @match          https://*.ingress.com/mission/*
+// @match          http://*.ingress.com/mission/*
+// @grant          none
+// ==/UserScript==
