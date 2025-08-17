3d514c782f6faf6baad744beba0d41c0c3aa294b
Michael Hartley <mike40033@gmail.com>
2025-08-17 15:17:54 +0800

    Create drone-flight-planner.meta.js

---
+++ plugins/drone-flight-planner/drone-flight-planner.meta.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @id             iitc-plugin-drone-planner@57Cell
+// @name           IITC Plugin: 57Cell's Drone Flight Planner
+// @version        1.0.0.20250817
+// @description    Plugin for planning drone flights in IITC
+// @author         57Cell (Michael Hartley) and ChatGPT 4.0
+// @category       Layer
+// @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
+// @updateURL      https://github.com/mike40033/iitc-57Cell/raw/master/plugins/drone-flight-planner/drone-flight-planner.meta.js
+// @downloadURL    https://github.com/mike40033/iitc-57Cell/raw/master/plugins/drone-flight-planner/drone-flight-planner.user.js
+// @include        https://intel.ingress.com/*
+// @include        http://intel.ingress.com/*
+// @match          https://intel.ingress.com/*
+// @match          http://intel.ingress.com/*
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @include        https://*.ingress.com/mission/*
+// @include        http://*.ingress.com/mission/*
+// @match          https://*.ingress.com/mission/*
+// @match          http://*.ingress.com/mission/*
+// @grant        none
+// ==/UserScript==
